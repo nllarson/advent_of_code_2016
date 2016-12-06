f167631f0d7038bf62158f5fe3b0f3cbb544bdf6
Nick Larson <nicklas.larson@gmail.com>
2016-12-06 17:51:52 -0600

    refactor character count method

--- day6/Broadcast.groovy
+++ day6/Broadcast.groovy
@@ -1,23 +1,24 @@
 class Broadcast {
   def signal
 
+  Map countCharacters(index) {
+    def characterCounts = [:]
+
+    signal.each{ word ->
+      if (characterCounts[word[index]]) {
+        characterCounts[word[index]]++
+      } else {
+        characterCounts[word[index]] = 1
+      }
+    }
+
+    return characterCounts
+  }
+
   String message() {
     String message = ''
     signal.max{it.length()}.length().times { index ->
-      // println "Character: ${index}"
-
-      def characterCounts = [:]
-
-      signal.each{ word ->
-        if (characterCounts[word[index]]) {
-          characterCounts[word[index]]++
-        } else {
-          characterCounts[word[index]] = 1
-        }
-      }
-
-      // println "Character Counts: ${characterCounts.sort{a,b -> (a.value <=> b.value) * -1}}"
-      message += characterCounts.sort{a,b -> (a.value <=> b.value) * -1}*.key[0]
+      message += countCharacters(index).sort{a,b -> (a.value <=> b.value) * -1}*.key[0]
     }
 
     return message
@@ -26,23 +27,9 @@ class Broadcast {
   String modifiedMessage() {
     String message = ''
     signal.max{it.length()}.length().times { index ->
-      // println "Character: ${index}"
-
-      def characterCounts = [:]
-
-      signal.each{ word ->
-        if (characterCounts[word[index]]) {
-          characterCounts[word[index]]++
-        } else {
-          characterCounts[word[index]] = 1
-        }
-      }
-
-      // println "Character Counts: ${characterCounts.sort{a,b -> (a.value <=> b.value)}}"
-      message += characterCounts.sort{a,b -> (a.value <=> b.value)}*.key[0]
+      message += countCharacters(index).sort{a,b -> (a.value <=> b.value)}*.key[0]
     }
 
     return message
   }
-
 }
